561791483433f1423ceb2c7b1d7b98982b1adb70
Thomas Scharke <thomas.scharke@gmail.com>
2025-07-25 15:30:25 +0200

    refactor(wtlist): Simplify install script

--- install.sh
+++ install.sh
@@ -1,3 +1,6 @@
+# Define scripts to work with
+SCRIPTS=("wtadd.sh" "wtremove.sh" "wtclone.sh" "wtlist.sh")
+
 # Define the repository URL and the destination directory
 REPO_URL="https://github.com/tomups/worktrees-scripts.git"
 DEST_DIR="$HOME/.local/bin"
@@ -9,24 +12,20 @@ mkdir -p "$DEST_DIR"
 TEMP_DIR=$(mktemp -d)
 git clone "$REPO_URL" "$TEMP_DIR"
 
-# Copy the specified shell scripts to the destination directory
-cp "$TEMP_DIR"/wtadd.sh "$DEST_DIR"
-cp "$TEMP_DIR"/wtremove.sh "$DEST_DIR"
-cp "$TEMP_DIR"/wtclone.sh "$DEST_DIR"
+for script in "${SCRIPTS[@]}"; do
+  # Copy the specified shell scripts to the destination directory
+  cp "$TEMP_DIR/$script" "$DEST_DIR"
+
+  # Add execute permissions to the scripts
+  chmod +x "$DEST_DIR/$script"
+
+  # Add git aliases for each script
+  script_name=$(basename "$script" .sh)
+  git config --global alias."$script_name" "!$DEST_DIR/$script"
+done
 
 # Clean up the temporary directory
 rm -rf "$TEMP_DIR"
-
-# Add execute permissions to the scripts
-chmod +x "$DEST_DIR"/wtadd.sh
-chmod +x "$DEST_DIR"/wtremove.sh
-chmod +x "$DEST_DIR"/wtclone.sh
-
-# Add git aliases for each script
-for script in wtadd.sh wtremove.sh wtclone.sh; do
-  script_name=$(basename "$script" .sh)
-  git config --global alias.$script_name "!$DEST_DIR/$script"
-done
 
 # Notify the user
 echo "Shell scripts have been installed and configured as git aliases."
